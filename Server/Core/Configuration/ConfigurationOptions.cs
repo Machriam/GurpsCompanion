ba2edc5c34734c0b37cacc1dcfdcebdaacd8e3c2
Amadeus Lehmann <Amadeus Lehmann@DESKTOP J8Q46LU>
2021-01-14 23:03:45 +0100

    Add project files.

---
+++ Server/Core/Configuration/ConfigurationOptions.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace GurpsCompanion.Server.Core
+{
+    public class ConfigurationOptions
+    {
+        public const string Configuration = "Configuration";
+        public string DatabaseConnection { get; set; }
+    }
+}
