f0f69295f2f461d3abc7e83c545cfc0449a61ab1
Amadeus Lehmann <Amadeus Lehmann@DESKTOP J8Q46LU>
2021-03-01 22:44:39 +0100

    simplified enumconverter

--- Shared/Core/EnumConverter.cs
+++ Shared/Core/EnumConverter.cs
@@ -7,28 +7,25 @@ namespace GurpsCompanion.Shared.Core
 {
     public static class EnumConverter<T> where T : Enum
     {
-        private static readonly Dictionary<Type, Dictionary<string, T>> s_typeDictionary
-            = new Dictionary<Type, Dictionary<string, T>>();
+        private static Dictionary<string, T> s_typeDictionary
+            = new Dictionary<string, T>();
 
         public static IEnumerable<string> GetDescriptions()
         {
             InitializeDictionary();
-            return s_typeDictionary[typeof(T)].Keys;
+            return s_typeDictionary.Keys;
         }
 
         public static T ConvertTo(string value)
         {
             InitializeDictionary();
-            return s_typeDictionary[typeof(T)][value];
+            return s_typeDictionary[value];
         }
 
         private static void InitializeDictionary()
         {
-            if (!s_typeDictionary.ContainsKey(typeof(T)))
-            {
-                var typeDictionary = Enum.GetValues(typeof(T)).Cast<T>().ToDictionary(e => e.GetDescription());
-                s_typeDictionary.Add(typeof(T), typeDictionary);
-            }
+            if (s_typeDictionary.Count > 0) return;
+            s_typeDictionary = Enum.GetValues(typeof(T)).Cast<T>().ToDictionary(e => e.GetDescription());
         }
     }
 }
